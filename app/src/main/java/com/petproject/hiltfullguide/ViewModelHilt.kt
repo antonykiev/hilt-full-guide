e6a414d58d480d7f5be56a351de89c5634b35c8f
Anton Shcherbakov <1mailanton@gmail.com>
2024-09-18 13:33:15 +0200

    hilt: lazy injection

--- app/src/main/java/com/petproject/hiltfullguide/ViewModelHilt.kt
+++ app/src/main/java/com/petproject/hiltfullguide/ViewModelHilt.kt
@@ -2,6 +2,7 @@ package com.petproject.hiltfullguide
 
 import androidx.lifecycle.ViewModel
 import com.petproject.hiltfullguide.repository.Repository
+import dagger.Lazy
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import javax.inject.Named
@@ -9,10 +10,10 @@ import javax.inject.Named
 @HiltViewModel
 class ViewModelHilt @Inject constructor(
     @Named("repositoryAnother")
-    private val repository: Repository
+    private val repository: Lazy<Repository>
 ) : ViewModel() {
 
     fun userName(): String {
-        return repository.userName()
+        return repository.get().userName()
     }
 }
